4c96c764a4e4d5958ae1300bf2bab8fedc95d04a
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-07-29 07:36:32 -0700

    coba tema

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,15 +1,18 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-    palette: {
-        type: 'dark',
-        primary: {
-          main: '#5ae2c9',
-        },
-        secondary: {
-          main: '#EFCA08',
-        },
-      },
-});
-
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#455a64',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#80d8ff',
+    },
+  },
+}
+);
 export default theme;
