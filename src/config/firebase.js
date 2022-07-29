2031ecda6cc430728645734c1d996456d52aec73
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-07-29 08:17:16 -0700

    config database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,11 +15,14 @@ const firebaseConfig = {
   storageBucket: "dtsfinalproject-170.appspot.com",
   messagingSenderId: "445415447425",
   appId: "1:445415447425:web:059682af18fe0aa2962680",
-  measurementId: "G-Z7HNHMD7LF"
+  measurementId: "G-Z7HNHMD7LF",
+  databaseURL: "https://dtsfinalproject-170-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
-export {auth};
+
+export {auth, database};
