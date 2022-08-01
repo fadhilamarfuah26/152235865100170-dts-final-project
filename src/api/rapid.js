f5ed5ffe002cd4e3071b5a2e436ced8c4683d297
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-08-01 08:58:31 -0700

    coba api

---
+++ src/api/rapid.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_KEY = '43ebcd8ad8msh34e6dbe8d53b96ap124371jsn2f2446129c01';
+const baseUrl = 'https://japanese-alphabet.p.rapidapi.com/api/fullhiragana';
+
+const rapid = axios.create({
+  baseURL: baseUrl,
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+export default rapid;
