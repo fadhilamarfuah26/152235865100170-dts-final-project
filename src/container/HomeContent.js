0d7ef26e51ff9c6bf4b80016a6c95406989a84db
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-08-03 08:55:25 -0700

    cobaaaa

--- src/container/HomeContent.js
+++ src/container/HomeContent.js
@@ -1,4 +1,45 @@
 import rapid from '../api/rapid'
+import { useState, useEffect } from "react";
 
-const HomeContent = await rapid.get("/hiragana");
-console.log(HomeContent);
+
+export default function HomeContent() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await rapid.get(
+          '/hiragana'
+        );
+        setData(response.data);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>API Posts</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+        {data &&
+          data.map(({ id, title }) => (
+            <li key={id}>
+              <h3>{title}</h3>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
