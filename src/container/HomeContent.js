1c716313c763ec5c8edbc8da556f19105ff8bc6a
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-08-03 09:10:51 -0700

    mmm

--- src/container/HomeContent.js
+++ src/container/HomeContent.js
@@ -1,45 +1,16 @@
-import rapid from '../api/rapid'
-import { useState, useEffect } from "react";
+const axios = require("axios");
 
+const options = {
+  method: 'GET',
+  url: 'https://japanese-alphabet.p.rapidapi.com/api/all',
+  headers: {
+    'X-RapidAPI-Key': '43ebcd8ad8msh34e6dbe8d53b96ap124371jsn2f2446129c01',
+    'X-RapidAPI-Host': 'japanese-alphabet.p.rapidapi.com'
+  }
+};
 
-export default function HomeContent() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await rapid.get(
-          '/hiragana'
-        );
-        setData(response.data);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setData(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
-
-  return (
-    <div>
-      <h1>API Posts</h1>
-      {loading && <div>A moment please...</div>}
-      {error && (
-        <div>{`There is a problem fetching the post data - ${error}`}</div>
-      )}
-      <ul>
-        {data &&
-          data.map(({ japanese,romaji }) => (
-            <li key={japanese}>
-              <h3>{romaji}</h3>
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-}
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
