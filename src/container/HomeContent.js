9e3a7e5012a3adcc0b0ba51329a3d4fa0ac80516
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-08-04 20:18:44 -0700

    coba terus

--- src/container/HomeContent.js
+++ src/container/HomeContent.js
@@ -6,8 +6,7 @@ import rapid from '../api/rapid'
 
 const HomeContent = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  
 
   useEffect(() => {
     const getData = async () => {
@@ -15,15 +14,11 @@ const HomeContent = () => {
         const response = await rapid.get(
           'https://japanese-alphabet.p.rapidapi.com/api/hiragana'
         );
-        setData(response.data);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setData([]);
-      } finally {
-        setLoading(false);
-      }
-    };
+        setData(response.data.result);
+      } catch (error) {
+        console.log(error);
+    }
+  }
     getData();
   }, []);
 
@@ -36,11 +31,6 @@ const HomeContent = () => {
     
     <div>
       <h1>API Posts</h1>
-      {loading && (<div>A moment please...</div>)}
-      {error && (
-        <div>{`There is a problem fetching the post data - ${error}`}</div>
-      )}
-      
         {data &&
           (_.mapValues(data,([ name, content ]) => (
             <ul><li key={[name.value]}>
