8318aaec2ba24939831c6525fe8d1fe6fb9508bc
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-08-04 23:53:01 -0700

    lagiiii

--- src/api/rapid.js
+++ src/api/rapid.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 
+const API_KEY = '43ebcd8ad8msh34e6dbe8d53b96ap124371jsn2f2446129c01';
+const baseUrl = 'https://japanese-alphabet.p.rapidapi.com/api/hiragana';
+
 const rapid = axios.create({
-  url: 'https://japanese-alphabet.p.rapidapi.com/api',
-  headers: {
-    'X-RapidAPI-Key': '43ebcd8ad8msh34e6dbe8d53b96ap124371jsn2f2446129c01',
-    'X-RapidAPI-Host': 'japanese-alphabet.p.rapidapi.com',
-    "content-type": "application/json; charset=utf-8",
-  }
+  baseURL: baseUrl,
+  params: {
+    api_key: API_KEY,
+  },
 });
+
 
 export default rapid;
